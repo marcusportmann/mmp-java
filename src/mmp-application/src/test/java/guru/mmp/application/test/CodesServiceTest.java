31a0a27393314ade1e06fa2dd6f013d6c42c8719
Marcus Portmann <marcus@mmp.guru>
2015-12-27 12:39:32 +0200

    Updates

---
+++ src/mmp-application/src/test/java/guru/mmp/application/test/CodesServiceTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Marcus Portmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package guru.mmp.application.test;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import guru.mmp.common.test.ApplicationJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.*;
+
+import javax.inject.Inject;
+
+/**
+ * The <code>CodesServiceTest</code> class contains the implementation of the JUnit
+ * tests for the <code>CodesService</code> class.
+ *
+ * @author Marcus Portmann
+ */
+@RunWith(ApplicationJUnit4ClassRunner.class)
+public class CodesServiceTest
+{
+
+
+}
