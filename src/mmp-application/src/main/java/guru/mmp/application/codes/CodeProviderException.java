cc7da24e34ed59e24aa23cef6f9a964d53f5ff81
Marcus Portmann <marcus@mmp.guru>
2016-05-02 02:17:30 +0200

    Updates

--- src/mmp-application/src/main/java/guru/mmp/application/codes/CodeProviderException.java
+++ src/mmp-application/src/main/java/guru/mmp/application/codes/CodeProviderException.java
@@ -23,7 +23,7 @@ package guru.mmp.application.codes;
  * @author Marcus Portmann
  */
 @SuppressWarnings("unused")
-class CodeProviderException extends Exception
+public class CodeProviderException extends Exception
 {
   private static final long serialVersionUID = 1000000;
 
