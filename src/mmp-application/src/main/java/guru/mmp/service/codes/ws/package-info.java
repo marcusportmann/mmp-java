1b4fccb5465ddcda5f78141b78a146285be968fc
Marcus Portmann <marcus@mmp.guru>
2017-10-01 14:08:45 +0200

    Updates

--- src/mmp-application/src/main/java/guru/mmp/service/codes/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.codes.service.mmp.guru", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package guru.mmp.service.codes.ws;
